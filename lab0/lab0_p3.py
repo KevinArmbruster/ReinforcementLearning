61fdd668b694f850335141af80d110164d5e4d42
Kevin Armbruster <KevinArmbruster2013@gmail.com>
2022-11-06 14:35:09 +0100

    add lab0 problem 3 pytorch and RL

---
+++ lab0/lab0_p3.py
@@ -0,0 +1,82 @@
+import numpy as np
+import gym
+from collections import deque
+from random import sample
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+replay_buffer = deque(maxlen=100)
+
+
+class Net(nn.Module):
+
+    def __init__(self, states, actions):
+        super(Net, self).__init__()
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(states, 8)
+        self.fc2 = nn.Linear(8, actions)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+### CREATE RL ENVIRONMENT ###
+env = gym.make('CartPole-v0')  # Create a CartPole environment
+n = len(env.observation_space.low)  # State space dimensionality
+m = env.action_space.n  # Number of actions
+
+nn = Net(n, m)
+optim = torch.optim.Adam(nn.parameters(), lr=0.01)
+
+### PLAY ENVIRONMENT ###
+# The next while loop plays 5 episode of the environment
+for episode in range(5):
+    state = env.reset()  # Reset environment, returns initial state
+    done = False  # Boolean variable used to indicate if an episode terminated
+
+    while not done:
+        env.render()  # Render the environment (DO NOT USE during training of the labs...)
+        # action  = np.random.randint(m)   # Pick a random integer between [0, m-1]
+        state_tensor = torch.tensor([state], requires_grad=False)
+        action_tensor = nn.forward(state_tensor)
+        action = action_tensor.max(1)[1]
+        action = action.item()
+        print(f"state {state_tensor} -> actions {action_tensor} -> action {action}")
+
+        # The next line takes permits you to take an action in the RL environment
+        # env.step(action) returns 4 variables:
+        # (1) next state; (2) reward; (3) done variable; (4) additional stuff
+        next_state, reward, done, *_ = env.step(action)
+
+        replay = (state, action, reward, next_state, done)
+        replay_buffer.append(replay)
+
+        state = next_state
+
+
+        optim.zero_grad()
+        batch_size = 3
+        if len(replay_buffer) >= batch_size:
+            batch = sample(list(replay_buffer), batch_size)
+            states = np.array(list(zip(*batch))[0])
+            actions = torch.tensor(list(zip(*batch))[1])
+            actions = torch.reshape(actions, (-1, 1))
+
+            state_tensors = torch.tensor(states, requires_grad=False)
+            action_tensors = nn.forward(state_tensors)
+
+            z = torch.gather(action_tensors, 0, actions)
+            y = torch.zeros_like(z)
+
+            loss = torch.nn.functional.mse_loss(z, y)
+            loss.backward()
+            optim.step()
+            torch.nn.utils.clip_grad_norm(nn.parameters(), 1)
+
+
+# Close all the windows
+env.close()
+
