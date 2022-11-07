4c662ac201234a1dea4f38b7f87407051f3d5fd0
Kevin Armbruster <KevinArmbruster2013@gmail.com>
2022-11-07 18:16:13 +0100

    fix lab0 p3

--- lab0/lab0_p3.py
+++ lab0/lab0_p3.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-replay_buffer = deque(maxlen=100)
+replay_buffer = deque(maxlen=1000)
 
 
 class Net(nn.Module):
@@ -33,7 +33,7 @@ optim = torch.optim.Adam(nn.parameters(), lr=0.01)
 
 ### PLAY ENVIRONMENT ###
 # The next while loop plays 5 episode of the environment
-for episode in range(5):
+for episode in range(50):
     state = env.reset()  # Reset environment, returns initial state
     done = False  # Boolean variable used to indicate if an episode terminated
 
@@ -42,9 +42,8 @@ for episode in range(5):
         # action  = np.random.randint(m)   # Pick a random integer between [0, m-1]
         state_tensor = torch.tensor([state], requires_grad=False)
         action_tensor = nn.forward(state_tensor)
-        action = action_tensor.max(1)[1]
-        action = action.item()
-        print(f"state {state_tensor} -> actions {action_tensor} -> action {action}")
+        action = action_tensor.max(1)[1].item()
+        # print(f"state {state_tensor} -> actions {action_tensor} -> action {action}")
 
         # The next line takes permits you to take an action in the RL environment
         # env.step(action) returns 4 variables:
@@ -57,26 +56,37 @@ for episode in range(5):
         state = next_state
 
 
-        optim.zero_grad()
         batch_size = 3
         if len(replay_buffer) >= batch_size:
+            # set gradients to 0
+            optim.zero_grad()
+
+            # get batch for training
             batch = sample(list(replay_buffer), batch_size)
-            states = np.array(list(zip(*batch))[0])
-            actions = torch.tensor(list(zip(*batch))[1])
-            actions = torch.reshape(actions, (-1, 1))
+            batch = list(zip(*batch))
 
-            state_tensors = torch.tensor(states, requires_grad=False)
-            action_tensors = nn.forward(state_tensors)
+            # get states from batch
+            batch_states = np.array(batch[0])
+            states = torch.tensor(batch_states, requires_grad=False)
 
-            z = torch.gather(action_tensors, 0, actions)
-            y = torch.zeros_like(z)
+            # run NN for action prediction
+            action_predictions = nn(states)
 
-            loss = torch.nn.functional.mse_loss(z, y)
+            # zero vector like prediction - why??
+            target = torch.zeros_like(action_predictions, requires_grad=False)
+
+            # compute loss
+            loss = torch.nn.functional.mse_loss(action_predictions, target)
+
+            # compute gradients
             loss.backward()
+
+            # clip gradients
+            torch.nn.utils.clip_grad_norm_(nn.parameters(), 1.)
+
+            # perform backprop
             optim.step()
-            torch.nn.utils.clip_grad_norm(nn.parameters(), 1)
 
 
 # Close all the windows
 env.close()
-
